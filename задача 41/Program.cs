471e15f1bb29697e6fdc8b7245ce5264f229b812
ilia <ilia100@hotmail.com>
2023-02-07 00:09:58 +0300

    all done

---
+++ задача 41/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+
+
+
+int [] GetArray(string message)
+{
+    Console.WriteLine(message);
+    string text = Console.ReadLine();
+    char[] separators = new char[] { ' ', '.', ',', '_' };
+    int[] arr = text.Split(separators).Select(n => Convert.ToInt32(n)).ToArray();
+
+    return arr;
+   
+}
+
+void PrintArray(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+
+}
+
+void GetSum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) sum++;
+    }
+    Console.Write($" -> Введено {sum} числа(ел) больше 0");
+}
+
+int [] array = GetArray("введите числа в массив  и нажмите Enter");
+PrintArray(array);
+GetSum(array);
---
+++ задача 43/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+
+int Insert(string messege)
+{
+    System.Console.Write(messege);
+    int num = Convert.ToInt32(Console.ReadLine());
+    
+    return num;
+}
+
+
+void GetPoint (double k1, double b1, double k2, double b2)
+{
+
+double x=(b2-b1)/(k1-k2);
+double y=k1*(b2-b1)/(k1-k2)+b1;
+
+
+System.Console.WriteLine($"точка пересечения данных прямых -> ({x},{y})");
+}
+
+
+System.Console.WriteLine("введите данные в формулу (y = k1 * x + b1, y = k2 * x + b2):");
+int k1 =Insert("k1=");
+int b1 =Insert("b1=");
+int k2 =Insert("k2=");
+int b2 =Insert("b2=");
+
+
+GetPoint(k1,b1,k2,b2);
